a35b5606300d4b95c3e9a8fcd13a14b7d4412c4b
esse.jacques.co <essedansomon@gmail.com>
2024-09-12 12:03:58 +0000

    feat: design

---
+++ js/videos.js
@@ -0,0 +1,10 @@
+// Select elements here
+const video = document.getElementById('video');
+const videoControls = document.getElementById('video-controls');
+
+const videoWorks = !!document.createElement('video').canPlayType;
+if (videoWorks) {
+  video.controls = false;
+  videoControls.classList.remove('hidden');
+}
+
